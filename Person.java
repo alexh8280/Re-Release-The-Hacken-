b999e0bc0ad338308f6b51931ac73e88d65a7cb6
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-03-24 10:23:36 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,32 @@
+import java.time.LocalDateTime;
+
+public class Person extends Contact {
+
+	protected String email;
+	protected String dateOfBirth;
+
+	public Person(int contactId, String name, String number, LocalDateTime createdAt, String email,
+			String dateOfBirth) {
+		super(contactId, name, number, createdAt);
+		this.email = email;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	
+}
